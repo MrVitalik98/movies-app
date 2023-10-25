af0e2647f6c987188e43463596d61357c92d17d6
eltac-malik <eltacem@code.edu.az>
2023-10-25 14:29:03 +0400

    google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
 import type { AppProps } from 'next/app'
@@ -7,11 +9,32 @@ import "../styles/globals.scss"
 import Layout from '../components/Layout'
 import store from '../store'
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
 
 config.autoAddCss = false
 
 
 const MyApp:FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  // Listen for route changes to ensure GTM reloads on page navigation
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // Replace 'GTM-KHGWKWGL' with your GTM Container ID
+      window.dataLayer.push({ event: 'pageview', page: url });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
+
   return (
     <Provider store={store}>
       <Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,11 +7,32 @@ class MyDocument extends Document<FC> {
     return (
       <Html lang="en">
         <Head>
+           {/* Add your GTM code here */}
+           <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({ 'gtm.start': new Date().getTime(), event:'gtm.js' });
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KHGWKWGL');`,
+            }}
+          />
+
           <meta name="description" content="Here you can easily watch movies of all genres." />
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"></link>
         </Head>
         <body>
+          <noscript>
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KHGWKWGL"
+              height="0" 
+              width="0" 
+              style={{display:"none", visibility:"hidden"}}>
+            </iframe>
+          </noscript>
+
           <Main />
           <NextScript />
 
