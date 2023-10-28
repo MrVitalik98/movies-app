9273540d690f9f8719deb4a02c19211ed1d53e2c
vitalik <vitalikadilov@gmail.com>
2023-10-28 17:01:26 +0400

    meta keywords

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,8 @@ class MyDocument extends Document<FC> {
           <meta property="og:title" content="Top Movies and TV Shows Stats" />
           <meta property="og:description" content="Get in-depth statistics and insights into your favorite movies and TV shows. Discover trending films, ratings, cast details, and more" />
           <meta name="robots" content="index, follow" />
-
+          <meta name="keywords" content="movies statistics, TV shows data, film ratings, television show reviews, actor information, director details, genre analysis, box office stats, movie database, TV show database, entertainment industry, cinematic analysis, popular films, top TV series, on-screen trends, awards and nominations" />
+          
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"></link>
 
